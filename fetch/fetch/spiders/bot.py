b3c3cb6b5678a1645366e04d02ee342e7ba9d22f
Cosmin Tudor <cosmin2dor@gmail.com>
2020-03-14 20:37:14 +0200

    initial commit

---
+++ fetch/fetch/spiders/bot.py
@@ -0,0 +1,77 @@
+import scrapy
+import json
+import time
+
+
+def format_numerical(string):
+    try:
+        if string.strip() == "":
+            return "0"
+        else:
+            return string.strip().replace(',', '')
+    except AttributeError:
+        return "NAN"
+
+
+class Bot(scrapy.Spider):
+    name = "bot"
+
+    def start_requests(self):
+        urls = [
+            'https://www.worldometers.info/coronavirus/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        global_stats = response.css("div.maincounter-number span::text").getall()
+
+        global_cases = format_numerical(global_stats[0])
+        gloabl_deaths = format_numerical(global_stats[1])
+        global_recovered = format_numerical(global_stats[2])
+
+        rows = {}
+
+        for idx in range(1, 121):
+            base_path = "/html/body/div[2]/div[3]/div/div[1]/table/tbody[1]/tr[{}]/td[{}]//text()"
+            country = response.xpath(base_path.format(idx, 1)).getall()
+
+            if len(country) == 3:
+                country = country[1].strip()
+            else:
+                country = country[0].strip()
+
+            total_cases = format_numerical(response.xpath(base_path.format(idx, 2)).get())
+            new_cases = format_numerical(response.xpath(base_path.format(idx, 3)).get())
+            total_deaths = format_numerical(response.xpath(base_path.format(idx, 4)).get())
+            new_deaths = format_numerical(response.xpath(base_path.format(idx, 5)).get())
+            total_recovered = format_numerical(response.xpath(base_path.format(idx, 6)).get())
+            active_cases = format_numerical(response.xpath(base_path.format(idx, 7)).get())
+            critical = format_numerical(response.xpath(base_path.format(idx, 8)).get())
+            total_cases_by_1M = format_numerical(response.xpath(base_path.format(idx, 9)))
+            death_rate = str((float(total_deaths) / int(total_cases)) * 100.0) + ' %'
+
+            row = {
+                'total_cases': total_cases,
+                'new_cases': new_cases,
+                'total_deaths': total_deaths,
+                'new_deaths': new_deaths,
+                'total_recovered': total_recovered,
+                'active_cases': active_cases,
+                'critical': critical,
+                'total_cases_by_1M': total_cases_by_1M,
+                'death_rate': death_rate,
+            }
+
+            rows[country] = row
+
+        data = {
+            'global_cases': global_cases,
+            'gloabl_deaths': gloabl_deaths,
+            'global_recovered': global_recovered,
+            'countires': rows,
+            'timestamp': time.time()
+        }
+
+        with open('data.json', 'w') as outfile:
+            json.dump(data, outfile)
---
+++ webapp/app/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+import requests
+import json
+
+
+def get_client_ip(request):
+    return "188.26.27.31"
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def locate_user(request):
+    ip = get_client_ip(request)
+    api_key = "6fd4e0d631f6fb7fce324c7e928a3ddd"
+    endpoint = "http://api.ipstack.com/{}?access_key={}".format(ip, api_key)
+
+    r = requests.get(endpoint)
+    data = r.json()
+
+    return data['country_name'], data['location']['country_flag']
+
+
+def index_view(request):
+    country, flag = locate_user(request)
+
+    my_country = None
+
+    with open('../fetch/data.json') as json_file:
+        data = json.load(json_file)
+        try:
+            my_country = data['countires'][country]
+        except KeyError:
+            pass
+
+        active_cases = int(data['global_cases'].replace(',', '')) - int(data['global_recovered'].replace(',', '')) - int(data['gloabl_deaths'].replace(',', ''))
+
+        return render(request, 'index.html', {'location': country,
+                                              'flag': flag,
+                                              'global_cases': data['global_cases'],
+                                              'global_deaths': data['gloabl_deaths'],
+                                              'global_recovered': data['global_recovered'],
+                                              'global_active': active_cases,
+                                              'my_country_total_cases': my_country['total_cases'],
+                                              'my_country_new_cases': my_country['new_cases'],
+                                              'my_country_new_deaths': my_country['new_deaths'],
+                                              'my_country_total_deaths': my_country['total_deaths'],
+                                              'countires': data['countires']})
