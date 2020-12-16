f4d7a816c0444ba751e37f6fee720e70451116ef
cosmin2dor <cosmin2dor@gmail.com>
2020-12-16 17:50:44 +0000

    Web crawler updates

--- emergency.sh
+++ emergency.sh
@@ -1,1 +1,2 @@
 docker run --rm -d -v /var/www/covid-status/static:/static -v /root/covidstatus.live/webapp/data.json:/django/data.json,shared -p 8000:8000 --name covid-django cosmin2dor/covid-django:stable
+docker cp /root/covidstatus.live/webapp/data.json.backup covid-django:/django/
