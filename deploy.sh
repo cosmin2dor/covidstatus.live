9f2ec6b5567a12335ba56fdb1f139cdbe2d5895b
Ubuntu <ubuntu@ip-172-31-86-44.ec2.internal>
2020-12-28 21:56:18 +0000

    Hide doctor messages and normalized map values

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 docker build -t covid-django webapp/
-docker tag covid-django cosmin2dor/covid-django
-docker push cosmin2dor/covid-django
+
+docker stop covid-django
+docker run --rm -d -v /var/www/covid-status/static:/static -v /home/ubuntu/covidstatus.live/webapp/data.json:/django/data.json,shared --network=django-redis -p 8000:8000 --name covid-django covid-django
+
+pkill -f updater.sh
+nohup bash fetch/updater.sh &
